cbda0f365c587bb12055b9cdd9c4d51ae7a5869f
bjzhh <510809889@qq.com>
2019-10-08 17:53:48 +0800

    用系统默认浏览器打开网页

--- MainRexel.py
+++ MainRexel.py
@@ -36,10 +36,10 @@ class MainMenu(QMainWindow):
         m2.addAction('CRM')
         m2.addAction('PAS')
         m2.addAction('ESS')
-        m2.addAction('OMS')
-        m2.addAction('IMC')
-        m2.addAction('Office365')
-        m2.addAction('Rexel website')
+        m2.addAction('Rexel Academy')
+        m2.addAction('Success Factor')
+        m2.addAction('Web Email')
+        m2.addAction('rexel.com.cn')
 
         m3 = bar.addMenu('Office')
         m3.addAction('Excel Split')
@@ -65,10 +65,22 @@ class MainMenu(QMainWindow):
         print(q.text(),' is ok')
         if q.text() == 'Quit':
             self.close()
-        elif q.text() == 'Office365':
+        elif q.text() == 'Web Email':
             webbrowser.open_new('http://outlook.office365.com')
-        elif q.text() == 'Rexel website':
+        elif q.text() == 'rexel.com.cn':
             webbrowser.open_new('http://www.rexel.com.cn')
+        elif q.text() == 'IFS':
+            webbrowser.open_new('http://jbosssvr01.rexel-cn.local:58080/')
+        elif q.text() == 'CRM':
+            webbrowser.open_new('http://10.219.120.35/Login/Index')
+        elif q.text() == 'PAS':
+            webbrowser.open_new('http://10.219.100.70:8080/pas/')
+        elif q.text() == 'ESS':
+            webbrowser.open_new('http://10.219.100.77/PlatinumHRM-ESS/logon.aspx')
+        elif q.text() == 'Success Factor':
+            webbrowser.open_new('https://performancemanager4.successfactors.com/login?company=Rexel&loginMethod=PWD#/login')
+        elif q.text() == 'Rexel Academy':
+            webbrowser.open_new('https://rexelacademy.lms.crossknowledge.com/interfaces/login.php')
 
 
 if __name__ == '__main__':
